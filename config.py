8f641930348cb50d96a8e20cbb6d4d85f65cc837
Damon <dittopower@gmail.com>
2020-07-20 18:23:03 +1000

    Update config.py

--- config.py
+++ config.py
@@ -20,6 +20,11 @@ class config:
         with open('config.json', 'r') as file:
             self.filePath = path
             self.config = json.load(file)
+    
+    def __getKeyUpper__(self,key:str):
+        if (self.config[key]):
+            return self.config[key].upper()
+        return None
 
     def discordBotKey(self) -> str:
         return self.config[self.discord_bot_key]
@@ -28,19 +33,19 @@ class config:
         return self.config[self.admin_users]
 
     def default_Role(self) -> str:
-        return self.config[self.default_server_admin_role]
+        return self.__getKeyUpper__(self.default_Role)
 
     def defaultAdminRole(self) -> str:
-        return self.config[self.default_server_role]
+        return self.__getKeyUpper__(self.default_server_admin_role)
 
-    def getServers(self):
+    def getServers(self) -> list:
         return self.config[self.server_list]
 
     def getServer(self, i: int):
         return self.config[self.server_list][i]
 
     def getServerName(self, i: int) -> str:
-        return self.config[self.server_list][i][self.server_name]
+        return self.__getKeyUpper__(self.server_name)
 
     def getServerLauncher(self, i: int) -> str:
         return self.config[self.server_list][i][self.server_launcher]
@@ -55,7 +60,7 @@ class config:
         return self.config[self.server_list][i][self.server_save_command]
 
     def getServerRole(self, i: int) -> str:
-        return self.config[self.server_list][i][self.server_role]
+        return self.__getKeyUpper__(self.server_role)
 
     def getServerAdminRole(self, i: int) -> str:
-        return self.config[self.server_list][i][self.server_admin_role]
+        return self.__getKeyUpper__(self.server_admin_role)
