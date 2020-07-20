d642c440ccc7c7b455a2207b68c817c30f5ff91d
Damon <dittopower@gmail.com>
2020-07-20 23:36:01 +1000

    Add MANIFEST

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+   name='DiscordantDeamonPy',
+   version='1.0',
+   description='Game Server Management Bot for Discord',
+   author='Deamon',
+   author_email='dev@deamon.info',
+   packages=[],
+   install_requires=['discord'], #external packages as dependencies
+)
