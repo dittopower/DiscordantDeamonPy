b346327f04c8f79e120350f7877831ad5a686595
Damon <dittopower@gmail.com>
2020-07-20 23:57:39 +1000

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,11 +1,22 @@
-from setuptools import setup
+from setuptools import setup, find_packages
 
 setup(
-   name='DiscordantDeamonPy',
-   version='1.0',
-   description='Game Server Management Bot for Discord',
-   author='Deamon',
-   author_email='dev@deamon.info',
-   packages=[],
-   install_requires=['discord.py'], #external packages as dependencies
+    name='DiscordantDeamonPy',
+    version='1.0',
+    description='Game Server Management Bot for Discord',
+    author='Deamon',
+    author_email='dev@deamon.info',
+    # scripts=[],
+    # entry_points={
+    #     "console_scripts": [
+    #         "bot = bot2:main_func"
+    #     ]
+    # },
+    packages=find_packages(),
+    install_requires=['discord.py'],
+    # package_data={
+    #     "": ["*.py", "*.json"]
+    # },
+    url="https://github.com/dittopower/DiscordantDeamonPy",
+    keywords="discord server host management game_server"
 )
